9631754845d7dd862879eb435c09a8c12d661ce6
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-11 12:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ class Person {
 	get name(){
 		return this.name;
 	}
-
+	set name(newName){
+		this.name=newName;
+	}
+	
+	get age(){
+		return this.age;
+	}
 	set age(newAge){
 		this.age=newAge;
 	}
